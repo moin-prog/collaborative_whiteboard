aedd1f49f87677b35abcb80dc5f0016acf127fd1
Moin Khan <moinkhan18457@gmail.com>
2025-07-06 21:05:56 +0530

    Update cleanupRooms.js

--- server/cron/cleanupRooms.js
+++ server/cron/cleanupRooms.js
@@ -4,7 +4,7 @@ const Room = require("../models/room");
 async function cleanupInactiveRooms() {
   const cutoff = new Date(Date.now() - 24 * 60 * 60 * 1000); // 24 hours ago
   const result = await Room.deleteMany({ lastActivity: { $lt: cutoff } });
-  console.log(`🧹 Cleaned up ${result.deletedCount} inactive rooms`);
+  console.log(`Cleaned up ${result.deletedCount} inactive rooms`);
 }
 
 module.exports = cleanupInactiveRooms;
